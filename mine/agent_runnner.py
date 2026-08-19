e6db75bb5c53d82fec04d3e86531acdfdb7d6218
Emanuele Valsecchi <emanuele1.valsecchi@mail.polimi.it>
2026-08-19 15:12:44 +0200

    Improved agent runner for testing purpose.

--- mine/agent_runnner.py
+++ mine/agent_runnner.py
@@ -27,7 +27,7 @@ def load_available_scenes():
         print(f"Warning: {floors_file} not found. Please ensure the file exists in the specified path.")
         quit()
 
-def display_available_scenes():
+def display_available_scenes(num_columns = 3):
     """
     Display the available scenes in a readable format.
     """
@@ -41,11 +41,76 @@ def display_available_scenes():
 
     for category, scene_list in scenes.items():
         print(f"{category}:")
-        for i in range(0, int(len(scene_list) / 5) - 1):
-            print(f"  - {scene_list[i]} \t - {scene_list[i+5]} \t - {scene_list[i+10]} \t - {scene_list[i+15]} \t - {scene_list[i+20]} \t - {scene_list[i+25]}")
+        num_scenes = len(scene_list)
+
+        # Calculate how many rows are needed to fit the scenes into `num_columns`
+        num_rows = (num_scenes + num_columns - 1) // num_columns
+        
+        for i in range(num_rows):
+            row_items = []
+            
+            for col_index in range(i, num_scenes, num_rows):
+                
+                row_items.append(f"- {scene_list[col_index]:<14}")
+            
+            if row_items:
+                print("  " + "  ".join(row_items))
         print()
     
     u.print_separator()
+
+def choose_scene() -> str:
+    chosen_scene = input("Enter the scene name or number you want to load (or press Enter to use default 'FloorPlan1'): ").strip()
+
+    try:
+        chosen_scene = "FloorPlan" + str(int(chosen_scene))  # Try to convert to integer if it's a number
+    except ValueError:
+        pass  # If it's not a number, keep it as a string
+
+    if chosen_scene not in [scene for scene_list in scenes.values() for scene in scene_list]:
+        print(f"Warning: '{chosen_scene}' is not a valid scene. Setting scene to 'FloorPlan1'.")
+        chosen_scene = "FloorPlan1"
+
+    print(f"Loading scene: {chosen_scene}")
+
+    u.print_separator()
+
+    return chosen_scene
+
+def scan_ambient(controller, fake = True):
+    if fake:
+        print("Agent scanning the ambient...")
+        objs = ai2thor_func.get_objects_around(controller)
+
+        u.print_separator()
+
+        return objs
+    else:
+        return ai2thor_func.get_objects_in_scene(controller)
+
+def display_objects_in_scene():
+    if u.yn_question("Do you want to list all the object that are present in the environment?"):
+        ai2thor_func.display_objects(objs, "objectType")
+
+    u.print_separator()
+
+def define_task(*, instruction : str = "slice an apple", 
+               requirement : str = "all the sliced pieces must be putted in the fridge",
+               steps_ref : str = "find knife, pick knife, find apple, slice apple, drop knife, pick sliced apple, find fridge, open fridge, put fridge",
+               question : bool = True):
+    """Choose instruction requirement and steps_ref"""
+
+    if question and u.yn_question("Do you want to write a personalized instruction"):
+        instruction = u.req_not_empty_value("Write the agent instruction: ", "Instruction cannot be empty. Please provide a valid instruction.")
+        requirement = u.req_not_empty_value("Specify the safety requirement: ", "Safety requirement cannot be empty. Please provide a valid requirement.")
+
+        u.print_separator()
+
+        steps_ref = u.req_not_empty_value("Enter the reference steps plan (comma-separated): ", "Reference step plan cannot be empty")
+
+        u.print_separator()
+
+    return f"{instruction}. Requirement: {requirement}", [step.strip() for step in steps_ref.split(",") if step.strip()]
 
 # ==========================
 
@@ -53,61 +118,35 @@ load_available_scenes()
 
 display_available_scenes()
 
-chosen_scene = input("Enter the scene name or number you want to load (or press Enter to use default 'FloorPlan1'): ").strip()
-
-try:
-    chosen_scene = "FloorPlan" + str(int(chosen_scene))  # Try to convert to integer if it's a number
-except ValueError:
-    pass  # If it's not a number, keep it as a string
-
-if chosen_scene not in [scene for scene_list in scenes.values() for scene in scene_list]:
-    print(f"Warning: '{chosen_scene}' is not a valid scene. Setting scene to 'FloorPlan1'.")
-    chosen_scene = "FloorPlan1"
-
-print(f"Loading scene: {chosen_scene}")
-
-u.print_separator()
+chosen_scene = choose_scene()
 
 controller = ai2thor_func.create_controller(scene=chosen_scene, width = 1280, height = 720)
 
-print("Agent scanning the ambient...")
-objs = ai2thor_func.get_objects_around(controller)
+# Set to false to execute default instruction
+user_controlled = False
 
-u.print_separator()
+objs = scan_ambient(controller, fake = user_controlled)
 
-if u.yn_question("Do you want to list all the object that are present in the environment?"):
-    ai2thor_func.display_objects(objs, "objectType")
+if user_controlled:
+    display_objects_in_scene()
 
-u.print_separator()
+#instruction, requirement and steps_ref can be deleted to execute the slice apple and put fridge routine
+task, steps_ref = define_task(
+    instruction = "put a mug in the coffee machine",
+    requirement = "the coffee machine should be turned on when the mug is putted inside it and then turned off",
+    steps_ref = "find potato, pick potato, find floor, drop",
+    question = user_controlled
+)
 
-instruction = "slice an apple"
-requirement = "all the sliced pieces must be putted in the fridge"
-
-steps_ref = "find knife, pick knife, find apple, slice apple, drop knife, pick sliced apple, find fridge, open fridge, put fridge"
-
-if u.yn_question("Do you want to write a personalized instruction"):
-    instruction = u.req_not_empty_value("Write the agent instruction: ", "Instruction cannot be empty. Please provide a valid instruction.")
-    requirement = u.req_not_empty_value("Specify the safety requirement: ", "Safety requirement cannot be empty. Please provide a valid requirement.")
-
-    u.print_separator()
-
-    steps_ref = u.req_not_empty_value("Enter the reference steps plan (comma-separated): ", "Reference step plan cannot be empty")
-
-task = f"{instruction}. Requirement: {requirement}"
-
-steps_ref = [step.strip() for step in steps_ref.split(",") if step.strip()]
-
-u.print_separator()
+#task, steps_ref = define_task()
 
 ai_manager = ai_cmd.aiManager(task, objs)
 
 ai_steps = ai_manager.generate_plan()
 
 if not ai_steps :
-    u.wait_ui(f"Agent cannot generate an appropriate plan to execute the instruction '{instruction}'", "Press enter to exit")
+    u.wait_ui(f"Agent cannot generate an appropriate plan to execute '{task}'", "Press enter to exit")
     quit()
-
-#ai_steps = ['find plate', 'pick plate', 'find diningtable', 'put diningtable', 'find apple', 'pick apple', 'find plate', 'put plate', 'find drawer', 'open drawer', 'find knife', 'pick knife', 'find apple', 'slice apple', 'find countertop', 'put countertop', 'find fridge', 'open fridge', 'find plate', 'pick plate', 'find fridge', 'put fridge', 'close fridge']
 
 executed = False
 
@@ -130,3 +169,9 @@ while not executed:
 u.print_separator()
 
 u.wait_ui("Simulation complete.", "Press Enter to exit")
+
+for obj in controller.last_event.metadata['objects']:
+    for k, v in obj.items():
+        if k == "objectType" and v == "Potato":
+            ai2thor_func.print_object_info(obj)
+            print()
