ff717d5fa6c24242dd80c3dbd1f17daddaa58ebd
Emanuele Valsecchi <emanuele1.valsecchi@mail.polimi.it>
2026-09-14 16:03:54 +0200

    Completly redefined ACTIONS available and LIQUID definition through an Enum

--- ai2_thor_task.py
+++ ai2_thor_task.py
@@ -1,32 +1,4 @@
-agent_action = ("find", "pick", "put", "drop", "throw", "moveheldback", "moveheldleft", "moveheldright", "moveheldup", "movehelddown", "pour", "push", "pull", "open", "close", "break", "cook", "slice", "turnon", "turnoff", "dirty", "clean", "fillliquid", "emptyliquid")
-
-liquid_available = ("coffee", "wine", "water")
-
-# Constants for agent actions
-FIND = agent_action[0]
-PICK = agent_action[1]
-PUT = agent_action[2]
-DROP = agent_action[3]
-THROW = agent_action[4]
-MOVEHELDBACK = agent_action[5]
-MOVEHELDLEFT = agent_action[6]
-MOVEHELDRIGHT = agent_action[7]
-MOVEHELDUP = agent_action[8]
-MOVEHELDDOWN = agent_action[9]
-POUR = agent_action[10]
-PUSH = agent_action[11]
-PULL = agent_action[12]
-OPEN = agent_action[13]
-CLOSE = agent_action[14]
-BREAK = agent_action[15]
-COOK = agent_action[16]
-SLICE = agent_action[17]
-TURNON = agent_action[18]
-TURNOFF = agent_action[19]
-DIRTY = agent_action[20]
-CLEAN = agent_action[21]
-FILLLIQUID = agent_action[22]
-EMPTYLIQUID = agent_action[23]
+from enum import Enum
 
 def get_action_from_cmd( command : str ) -> str:
     return command.strip().split(" ", 1)[0].lower().strip()
@@ -34,20 +6,61 @@ def get_action_from_cmd( command : str ) -> str:
 def get_subjects_from_cmd( command : str ) -> tuple[str, ...]:
     return [subject for subject in command.split()[1:]]
 
-def get_available_liquids():
-    return liquid_available
+class LIQUID(Enum):
+    COFFEE = "coffee"
+    WINE = "wine"
+    WATER = "water"
 
-def is_action( action : str ) -> bool:
-    return action in agent_action
+    @classmethod
+    def is_liquid(cls, liquid: str) -> bool:
+        """Checks if a string matches any available liquid."""
+        return any(liquid == item.value for item in cls)
 
-def is_liquid( liquid : str ) -> bool:
-    return liquid in liquid_available
+    def get_all():
+        return tuple(l.value for l in LIQUID)
 
-def get_no_object_requested_actions() -> tuple[str]:
-    return (DROP, THROW, MOVEHELDBACK, MOVEHELDLEFT, MOVEHELDRIGHT, MOVEHELDUP, MOVEHELDDOWN)
+class ACTIONS(Enum):
+    FIND = ("find", 1)
+    PICK = ("pick", 1)
+    PUT = ("put", 1)
+    DROP = ("drop", 0)
+    THROW = ("throw", 0)
+    MOVEHELDBACK = ("moveheldback", 0)
+    MOVEHELDLEFT = ("moveheldleft", 0)
+    MOVEHELDRIGHT = ("moveheldright", 0)
+    MOVEHELDUP = ("moveheldup", 0)
+    MOVEHELDDOWN = ("movehelddown", 0)
+    POUR = ("pour", 0)
+    PUSH = ("push", 1)
+    PULL = ("pull", 1)
+    OPEN = ("open", 1)
+    CLOSE = ("close", 1)
+    BREAK = ("break", 1)
+    COOK = ("cook", 1)
+    SLICE = ("slice", 1)
+    TURNON = ("turnon", 1)
+    TURNOFF = ("turnoff", 1)
+    DIRTY = ("dirty", 1)
+    CLEAN = ("clean", 1)
+    FILLLIQUID = ("fillliquid", 2)
+    EMPTYLIQUID = ("emptyliquid", 1)
 
-def get_one_object_requested_actions() -> tuple[str]:
-    return (FIND, PICK, PUT, PUSH, OPEN, CLOSE, BREAK, COOK, SLICE, TURNON, TURNOFF, DIRTY, CLEAN, EMPTYLIQUID)
+    def __new__(cls, 
+            action: str, 
+            objects_req: int):
+        obj = object.__new__(cls)
+        obj._value_ = action
+        obj.objects_required = objects_req
+        return obj
 
-def get_two_objects_requested_actions() -> tuple[str]:
-    return (FILLLIQUID)
+    @classmethod
+    def is_action( cls, action : str ) -> bool:
+        """Checks if a string matches any available action."""
+        return any(action == item.value for item in cls)
+
+    @classmethod
+    def get_actions_with_objects_requested(cls, count : int) -> tuple[str]:
+        return tuple(item.value for item in cls if item.objects_required == count)
+
+    def __str__(self) -> str:
+        return self._value_
