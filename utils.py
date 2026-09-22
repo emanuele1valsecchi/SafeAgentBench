cf180302c1781b41f8b7526265c7f67e33882d57
Emanuele Valsecchi <emanuele1.valsecchi@mail.polimi.it>
2026-09-22 13:16:26 +0200

    Implemented a log system to store logs into a dedicated file

--- utils.py
+++ utils.py
@@ -1,3 +1,38 @@
+# === LOG HANDLER === #
+import logging
+
+logging.basicConfig(
+    filename='simulation_results.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s'
+)
+
+def save_in_log(text : str = None):
+    logging.info(text)
+
+def print_log(text : str = None, save_to_log : bool = True):
+    if not text:
+        return
+
+    print(text)
+
+    if save_to_log:
+        save_in_log(text)
+
+def input_log(text: str = None, save_to_log: bool = True) -> str:
+    """Acts like standard input() but logs the prompt + user response."""
+    if not text:
+        return
+
+    user_response = input(text)
+    
+    if save_to_log:
+        logging.info(f"{text}{user_response}")
+        
+    return user_response
+
+# === USEFUL FUNCTIONS === #
+
 def format_column_content(*, 
         content : list[str], 
         column : int = 3,
@@ -23,44 +58,59 @@ def format_column_content(*,
 
     return content_formatted
 
-def print_separator(char_numb = 60):
-    print("\n" + "=" * char_numb + "\n")
+def print_separator(character : str = "=", title : str = None, char_numb = 60, save_to_log : bool = True):
+    if not title:
+        print_log(
+            text = "\n" + character * char_numb + "\n", 
+            save_to_log = save_to_log)
+    else:
+        char_numb = int((char_numb - len(title) - 4) / 2)
+        print_log(
+            text = "\n" + character * char_numb + f" [{title}] " + character * char_numb + "\n", 
+            save_to_log = save_to_log)
 
-def yn_question(question: str) -> bool:
+def yn_question(question: str, save_to_log : bool = True) -> bool:
     resp = ""
 
     while not resp:
-        resp = input( f"(Y/n) {question} : " ).strip()
+        resp = input_log(text = f"(Y/n) {question} : ", save_to_log = save_to_log).strip()
 
         if resp.lower() == "y" or resp.lower() == "yes":
             return True
         elif resp.lower() == "n" or resp.lower() == "no":
             return False
         else:
-            print(f"\nAnswer can only be 'y', 'yes', 'n' or 'no', cannot accept '{resp}'\n")
+            print_log(text = f"\nAnswer can only be 'y', 'yes', 'n' or 'no', cannot accept '{resp}'\n", save_to_log = save_to_log)
             resp = ""
             continue
 
     return False
 
-def req_not_empty_value(question: str, error_message: str="The value inserted is not allowed, please provide a valid input"):
+def req_not_empty_value(
+        question: str, 
+        error_message: str="The value inserted is not allowed, please provide a valid input",
+        save_to_log : bool = True
+    ):
     d = ""
 
     while not d:
-        d = input( question ).strip()
+        d = input_log(text = question, save_to_log = save_to_log).strip()
 
         if d == "":
-            print(f"{error_message} \n")
+            print_log(text = f"{error_message} \n", save_to_log = save_to_log)
             d = ""
 
     return d
 
-def wait_ui(text : str = "", end_message: str = "Press [Enter] to continue"):
+def wait_ui(
+        text : str = "", 
+        end_message: str = "Press [Enter] to continue",
+        save_to_log : bool = True):
     """Wait for user input: wait for enter key pressed by the user"""
     if end_message:
-        input (f"{text}\n{end_message}")
+        input_log(text=f"{text}\n{end_message}", save_to_log=save_to_log)
     else:
-        input(f"{text}")
+        input_log(text=f"{text}", save_to_log=save_to_log)
 
 def quit_program(text : str = "", end_message: str = "Press [Enter] to close the program"):
     wait_ui(text, end_message)
